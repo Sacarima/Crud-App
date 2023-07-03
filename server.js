a069d6965c9a127e378368e083c57a910f5aacd4
Sacarima <caroline12alex@yahoo.com>
2023-07-03 18:20:54 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+console.log("this is a test!")
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.listen(3000, function() {
+    console.log('listening....')
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.post('/quotes', (req, rep) => {
+    console.log(req.body)
+})
