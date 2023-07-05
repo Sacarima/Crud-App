11f7b063ea336085699732fdd79e2d892a7dbb0d
Sacarima <caroline12alex@yahoo.com>
2023-07-05 05:38:00 +0200

    Cloud server MongoDb setup

--- server.js
+++ server.js
@@ -4,7 +4,13 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const MongoClient = require('mongodb').MongoClient
+const connectionString = 'mongodb+srv://joaoaleixo:WWWiMRUtTKX5LhU7@sacarima.sxxpohm.mongodb.net/'
 
+
+MongoClient.connect(connectionString, (err, client) => {
+    if(err) return console.error(err)
+        console.log('Connected to DataBase')
+})
 app.use(bodyParser.urlencoded({extended: true}))
 
 app.listen(3000, function() {
