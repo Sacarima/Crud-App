09ead84d3c89f8dba89cbccf5b45598c3bab0005
Sacarima <caroline12alex@yahoo.com>
2023-07-11 14:02:10 +0200

    getting information back from the db

--- server.js
+++ server.js
@@ -13,9 +13,15 @@ MongoClient.connect(connectionString, {useUnifiedTopology: true} )
     console.log('Connected to MongoDB')
     const db = client.db('star-wars-quotes')
     const quotesCollection = db.collection('quotes')
+    app.set('view engine', 'ejs')
     app.use(bodyParser.urlencoded({extended: true}))
     app.get('/', (req, res) => {
-        res.sendFile(__dirname + '/index.html')
+        quotesCollection.find().toArray()
+            .then(results => {
+                console.log(results)
+            })
+            .catch(error => console.error(error))
+        res.render('index.ejs', {})
     })
     app.post('/quotes', (req, res) => {
         quotesCollection.insertOne(req.body)
