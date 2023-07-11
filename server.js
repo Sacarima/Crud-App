e89340dbe697fa10c05962608d72671424bd8c30
Sacarima <caroline12alex@yahoo.com>
2023-07-11 12:25:23 +0200

    remove gitignore files

--- server.js
+++ server.js
@@ -11,22 +11,28 @@ const connectionString = 'mongodb+srv://joaosacarima:GjzhdFRj31UK24MV@cluster0.l
 MongoClient.connect(connectionString, {useUnifiedTopology: true} )
 .then(client => {
     console.log('Connected to MongoDB')
-    let db = client.db('star-wars-quotes')
-    let = quotesCollection = db.collection('quotes')
+    const db = client.db('star-wars-quotes')
+    const quotesCollection = db.collection('quotes')
     app.use(bodyParser.urlencoded({extended: true}))
     app.get('/', (req, res) => {
         res.sendFile(__dirname + '/index.html')
     })
-    app.post('/quotes', (req, rep) => {
-        console.log(req.body)
+    app.post('/quotes', (req, res) => {
+        quotesCollection.insertOne(req.body)
+        .then(result => {
+            console.log(result)
+            res.redirect('/')
+        })
+        
+        .catch(error => console.error(error))
     })
     app.listen(3000, function() {
         console.log('listening....')
     })
+      
 
-}).catch(() => {
-    console.error('error')
 })
+
 
 
 
